bbf91c8fdaa1298c8811c75a63c4011f6f794f9f
Ben Smith <23388317+Bensmith9722@users.noreply.github.com>
2021-11-03 14:22:30 -0500

    Add signup input fields
    - First name
    - Last name
    - Email
    - Password
    - Password confirmation

--- event-finder-web-app/src/components/SignUp.jsx
+++ event-finder-web-app/src/components/SignUp.jsx
@@ -1,5 +1,60 @@
-function SignUp () {
+import { useState } from "react";
 
+const emptyAccountInfo = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    passwordConfirmation: ""
+}
+
+function SignUp () {
+    const [accountInfo, setAccountInfo] = useState(emptyAccountInfo);
+    
+    const onChange = (event) => {
+        const {name, value} = event.target;
+
+        setAccountInfo((prevAccountInfo) => {
+            return {
+                ...prevAccountInfo,
+                [name]: value
+            }
+        });
+    }
+
+    const onSubmit = (event) => {
+
+        console.log(accountInfo);
+
+        // Clear input fields
+        setAccountInfo(emptyAccountInfo);
+        event.preventDefault();
+    } 
+
+    return (
+        <>
+            <form onSubmit={onSubmit}>
+                <div className="form-group">
+                    <label htmlFor="">Name</label>
+                    <input className="form-control" onChange={onChange} type="text" value={accountInfo.firstName} name="firstName" placeholder="John" />
+                    <input className="form-control" onChange={onChange} type="text" value={accountInfo.lastName} name="lastName" placeholder="Doe"/>
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="">Email</label>
+                    <input className="form-control" onChange={onChange} type="email" value={accountInfo.email} name="email" placeholder="JohnDoe@example.com" />
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="">Password</label>
+                    <input className="form-control" onChange={onChange} type="password" value={accountInfo.password} name="password" placeholder="Password" />
+                    <input className="form-control" onChange={onChange} type="password" value={accountInfo.passwordConfirmation} name="passwordConfirmation" placeholder="Re-typed Password" />
+                </div>
+
+                <button type="submit" className="btn btn-dark btn-primary">SignUp</button>
+            </form>
+        </>
+    );
 }
 
 export default SignUp;
