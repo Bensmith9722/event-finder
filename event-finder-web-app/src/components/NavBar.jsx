36df3d7cd2352def1d887409a04006d007d6f9f5
Ben Smith <23388317+Bensmith9722@users.noreply.github.com>
2021-11-03 23:39:43 -0500

    Add dynamic navbar using user state

--- event-finder-web-app/src/components/NavBar.jsx
+++ event-finder-web-app/src/components/NavBar.jsx
@@ -1,19 +1,37 @@
-import Logout from "./Logout";
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+
+import Logout from "./Logout";
+import { AuthContext } from "./AuthContext";
 
 function NavBar () {
-    return (
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-            <Link className="navbar-brand" to="/">Event Finder</Link>
+    const { user } = useContext(AuthContext);
+    console.log(user);
 
-            <div className="navbar-nav">
-                <Link className="nav-item nav-link" to="/signup">Sign Up</Link>
-                <Link className="nav-item nav-link" to="/login">Login</Link>
-                
-                <Logout />
-            </div>
-        </nav>
-    );
+    /* A user is logged in, show relevant navbar. */
+    if (user) {
+        return (
+            <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+                <Link className="navbar-brand" to="/">Event Finder</Link>
+
+                <div className="navbar-nav">
+                    <Logout />
+                </div>
+            </nav>
+        );
+    }
+    else {
+        return (
+            <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+                <Link className="navbar-brand" to="/">Event Finder</Link>
+
+                <div className="navbar-nav">
+                    <Link className="nav-item nav-link" to="/signup">Sign Up</Link>
+                    <Link className="nav-item nav-link" to="/login">Login</Link>
+                </div>
+            </nav>
+        );
+    }
 }
 
 export default NavBar;
